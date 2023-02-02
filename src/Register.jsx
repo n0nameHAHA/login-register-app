9a751ce85bfcf78f54ea9ddbafe46efdb875f5f8
Roma <topgame324@gmail.com>
2023-02-03 01:35:52 +0200

    1

---
+++ src/Register.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const Register = (props) => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+  return (
+    <div className="auth-form-container">
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Full Name</label>
+        <input name="name" id="name" placeholder="Full Name" />
+        <label htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="type your Email"
+          id="email"
+          name="email"
+        />
+        <label for="password">Password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="***** "
+          id="password"
+          name="password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <button className="link-btn" onClick={() => props.onFormSwitch('login')}>
+        Already have an account? Lg In here
+      </button>
+    </div>
+  );
+};
+
+export default Register;
